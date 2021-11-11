31458aca199881b91395a3ba798e77508d2a00e3
Nicholas A Perry <nicholasperrypro@gmail.com>
2021-11-11 15:28:11 -0500

    first commit, working on basic search function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  let baseURL = "https://pokeapi.co/api/v2/";
+  const [name, setName] = useState('');
+  let pokemon, firstEv, secondEv;
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick(e) {
+    e.preventDefault();
+    await getPokemonAndEvolution();
+  }
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container" style={{marginTop: 10}}>
+        <div className="row justify-center">
+          <div className='col-9'>
+            <input className="form-control" onChange={(e) => setName(e.target.value)} placeholder="mudkip" />
+          </div>
+          <div className="col-auto">
+            <button onClick={async () => handleClick} className="btn btn-primary form-control">Search</button>
+          </div>
+        </div>
+        {pokemon ?
+          <div className="row">
+            <h1>Results</h1>
+          </div> : null}
+      </div>
     </div>
   );
+
+  async function getPokemonAndEvolution() {
+    const res = await fetch(`pokemon/${name}`)
+    if(res.ok) {
+      pokemon = await res.json()
+      
+    }
+  }
 }
 
 export default App;
